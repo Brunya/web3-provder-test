a2044eecebbcc58fef476ec17beb59df52b450d1
Bruno <>
2021-03-15 13:22:31 +0100

    getCurrentBlock

--- check.js
+++ check.js
@@ -6,6 +6,13 @@ const web3 = new Web3(process.env.URL);
 
 const accounts = web3.eth.accounts.wallet.decrypt(wallet, process.env.PASSWORD);
 
+async function getCurrentBlock() {
+  const currentBlock = await web3.eth.getBlockNumber();
+  return currentBlock;
+}
+
+getCurrentBlock().then((data) => console.log(data));
+
 web3.eth.net
   .isListening()
   .then((a) => console.log(a))
